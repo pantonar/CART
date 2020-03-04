5959d0080b6cc40789c15233c183b84db440b591
Pablo vivid <pablo.antonarnal@vivideconomics.com>
2020-03-04 15:26:40 +0000

    starting method

--- cart.py
+++ cart.py
@@ -12,10 +12,19 @@ from numpy import genfromtxt
 directory = r'C:\Users\Spare 3\Desktop\kaggle\CART'
 os.chdir(directory)
 
-labels      =
+f           = open('spambase.names')
+labels      = f.readlines()[33:]
+labels      = [i.split(':')[0] for i in labels]
 my_data     = genfromtxt('spambase.data', delimiter=',')
-X           = my_data[-1]
-Y           = genfromtxt('spambase.data', delimiter=',')
+Y           = my_data[:,my_data.shape[1]-1].reshape((my_data.shape[0],1))
+X           = my_data[:,0:my_data.shape[1]-1]
 
-with open('spambase.data') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
+
+class CART:
+    
+    def __init__(self):
+        self.X = X
+        self.Y = Y
+        
+    def train(self, loss_func = 'entropy'):
+        
